ca4cf078b32186c60aabedfbf1d6cee6f6329f32
anderson-robertp <anderon-rp@hotmail.com>
2023-09-19 11:38:48 -0500

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Generate list
+        List<int>numberList = new List<int>();
+        int number = 1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.WriteLine("Enter a number:");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            numberList.Add(number);
+        }while (number != 0);
+        
+        //Calulate sum of list
+        // int prevNumber = 0; - Testing
+        int totalN = 0;
+        //int iteration = 1; - Testing
+        foreach (int n in numberList)
+        {
+            totalN = totalN + n;
+            //Console.WriteLine($"{iteration}:{totalN} = {prevNumber} + {n}"); - Testing
+            //prevNumber = n; - Testing
+            // iteration++; - Testing
+        }
+        Console.WriteLine($"The sum of the list is {totalN}.");
+
+        //Compute Average
+        float avg = (float(totalN)) / numberList.Count;
+        Console.WriteLine($"The average is {avg}.");
+
+        // Find Max
+        int max = 0;
+        foreach (int n in numberList)
+        {
+            if (max < n)
+            {
+                max = n;
+            }
+        }
+        Console.WriteLine($"The greatest number is {max}");
+
+        // Find smallest positive value
+        int spv = 100;
+        foreach (int n in numberList)
+        {
+            if (n > 0 && spv > n)
+            {
+                spv = n;
+            }
+        }
+        Console.WriteLine($"Smallest Positive value is {spv}");
+
+        // Sort list from smallest to largest
+        numberList.Sort();
+        for (int j =0; j < numberList.Count; j++)
+        {
+            int line = numberList[j];
+            Console.WriteLine(line);
+        }
     }
 }
