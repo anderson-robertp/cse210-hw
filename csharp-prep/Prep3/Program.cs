00fe8a72f76b02e00695d8c28fdeea478febeb2d
anderson-robertp <131836551+anderson-robertp@users.noreply.github.com>
2023-09-19 09:57:37 -0500

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Call random
+        Random randomGenerator = new Random();
+
+        // variables
+        str response == "yes"
+        int i = 0
+
+        //doWhile for gameplay
+        do
+        {
+            // Gen magic number
+            int magicNumber = randomGenerator.Next)1,100);
+
+            while (guess != magicNumber)
+            {
+                Console.WriteLine("What is your guess? ");
+                str guessStr = Console.ReadLine();
+                int guess = intParse.(guessStr);
+
+                if (guess = magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                i++
+            }
+
+            Console.WriteLine($"You guessed {i} times.");
+            Console.WriteLine();
+            Console.WriteLine("Would you like to play again? yes/no");
+            response = Console.ReadLine();
+
+        } while (response == "yes");
     }
 }
