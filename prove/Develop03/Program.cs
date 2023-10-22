836e080bb7bfb48a0e3dd92e38ec3a7476f043e5
anderson-robertp <anderon-rp@hotmail.com>
2023-10-21 22:11:12 -0500

    update to main

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,16 +8,30 @@ class Program
         Scripture scrip = new Scripture();
 
         while (exit != "quit"){
+            // Clear Console
             Console.Clear();
+            
+            // Call Display Scripture
             scrip.DisplayScripture();
+            
+            //Write blank line nd instructions
             Console.WriteLine("");
             Console.WriteLine("Press enter or type quit to exit");
+            
+            // Read user inpuit
             exit = Console.ReadLine();
+
+            //Call check within scripture class if it is completely hidden
+            //Store variable from method to determine what to do next
             string next = scrip.IsCompletelyHidden();
+            
+            //Id not completely hidden, hide next word
             if (next == "next"){
                 scrip.HideRandomWord();
             }
+            //Else quit program
             else{
+                Console.WriteLine("The scripture is completely hidden.");
                 exit = "quit";
             }
             
